a2ce2393facf1a43c2fce41a29eff1cde099ef5b
v.shilaev <valentin.shilaev@smk-systems.ru>
2025-07-25 03:56:02 +0300

    initial

---
+++ orchestrator-service/src/test/java/ru/shilaev/orchestratorservice/OrchestratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shilaev.orchestratorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
